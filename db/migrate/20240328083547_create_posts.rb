8e7b06870e871752871c6f2fc396008986576be0
Banyan083214 <gen083214@gmail.com>
2024-03-28 22:28:41 +0900

    new,createアクション

--- db/migrate/20240328083547_create_posts.rb
+++ db/migrate/20240328083547_create_posts.rb
@@ -1,9 +1,16 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :name
-      t.string :text
-      t.text :image
+      t.string :title, null: false
+      t.text :synopsis
+      t.string :image
+      t.integer :genre_id, null: false
+      t.string :author, null: false
+      t.date :publication_date
+      t.text :text, null: false
+      t.references :user, null: false, foreign_key: true
+
+      
       t.timestamps
     end
   end
