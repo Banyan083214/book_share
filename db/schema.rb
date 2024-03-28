79c06f7a7a1675335e3cc8a96d689ec1056491ba
Banyan083214 <gen083214@gmail.com>
2024-03-28 18:22:14 +0900

    マイグレーション作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_28_073854) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_28_083547) do
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "text"
+    t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
